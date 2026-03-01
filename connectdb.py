428a3f880ca993c8e76fd7735991e6a73c4a40bf
Brendan Reed <101766219+reedbc1@users.noreply.github.com>
2026-03-01 04:28:05 -0600

    Added functionality: - fetch data from catalog - check state with database - delete records not found in catalog from db - insert items in catalog not already in db

--- connectdb.py
+++ connectdb.py
@@ -3,8 +3,15 @@ from google.cloud.sql.connector import Connector, IPTypes
 import sqlalchemy
 from dotenv import load_dotenv
 import pymysql
-
+from fetchitems import get_data
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column, Integer, String
 from sqlalchemy import text
+from sqlalchemy import insert
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from sqlalchemy import delete
+
 
 # Load .env file
 load_dotenv()
@@ -26,7 +33,7 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     ]  # e.g. 'project:region:instance'
     db_user = os.environ["DB_USER"]  # e.g. 'my-db-user'
     db_pass = os.environ["DB_PASS"]  # e.g. 'my-db-password'
-    db_name = os.environ["DB_NAME"]  # e.g. 'my-database'
+    db_name = "items"
 
     ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
 
@@ -50,11 +57,64 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
     )
     return engine
 
+
+# connect engine
 engine = connect_with_connector()
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
 
-# next, try inserting data
+# define metadata_obj
+metadata_obj = MetaData()
 
-# then, try inserting data from fetchitems.py
+# init books table
+books = Table("books", metadata_obj, autoload_with=engine)
+
+# fetch and load records
+records = get_data()
+
+# all fetched ids
+ids = {item['id'] for item in records}
+
+# check to see if any records already exist in db
+stmt = select(books.c.id)
+
+with Session(engine) as session:
+    for row in session.execute(stmt):
+        if row[0] in ids:
+            ids.remove(row[0])
+        else:
+            # delete row in db where id = row[0]
+            stmt = delete(books).where(books.c.id == row[0])
+            session.execute(stmt)
+    session.commit()
+    
+filtered_records = [r for r in records if r['id'] in ids]
+
+# insert records not already in db
+with Session(engine) as session:
+    result = session.execute(
+        insert(books), filtered_records
+    )
+    session.commit()
+
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+# with engine.connect() as conn:
+#     result = conn.execute("SELECT id FROM books")
+# print(result)
+
+# with engine.connect() as conn:
+#     result = conn.execute(
+#         insert(books), records
+#     )
+#     conn.commit()
